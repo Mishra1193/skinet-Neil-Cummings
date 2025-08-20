70a6228b07e2d91fa88a001d0a8bb72e5f71a003
Shubham Mishra <shubham.mishra1@hitachids.com>
2025-08-19 20:24:31 -0700

    End of Section 10

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule, MatButtonModule, MatBadgeModule],
+  imports: [MatIconModule, MatButtonModule, MatBadgeModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
